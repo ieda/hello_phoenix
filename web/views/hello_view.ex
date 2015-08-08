63d39926cad4d993e79f59a3055b849e697f8ac4
Ryo IEDA <ieda@val.co.jp>
2015-08-08 23:22:55 +0900

    スケジュール設定の解釈ロジックを再修正、表示を簡潔にした。

--- web/views/hello_view.ex
+++ web/views/hello_view.ex
@@ -2,7 +2,7 @@ defmodule HelloPhoenix.HelloView do
   use HelloPhoenix.Web, :view
 
   def parse_query(query) do
-    String.split(query, ",", trim: true)
+    String.split(query, "\n", trim: true)
     |> Enum.map(&([min, hour, day, month, weekday, command] = String.split(&1, " ", parts: 6)))
     |> Enum.map(&(crontime_to_schedule(&1)))
   end
@@ -16,13 +16,15 @@ defmodule HelloPhoenix.HelloView do
         end
         <>
         case [month, day] do
-         [_, "*"] -> ""
+         ["*", "*"] -> ""
+         [_, "*"] -> "の"
          ["*", day] -> "毎月#{day}日"
          [_, day] -> "#{day}日"
         end
         <>
         case [day, hour] do
-         [_, "*"] -> ""
+         ["*", "*"] -> ""
+         [_, "*"] -> "の"
          ["*", hour] -> "毎日#{hour}時"
          [_, hour] -> "#{hour}時"
         end
@@ -32,7 +34,7 @@ defmodule HelloPhoenix.HelloView do
          ["*", min] -> "毎時#{min}分"
          [_, min] -> "#{min}分"
         end
-        <> "に#{command}を実行します。"
+        <> "に#{command}"
     end
   end
 end
