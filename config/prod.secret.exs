03545f8bbd943ac29268d9801457de96369e601c
Ryo IEDA <ieda@val.co.jp>
2015-08-09 10:04:59 +0900

    Herokuデプロイ用設定を追加

---
+++ config/prod.secret.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool
