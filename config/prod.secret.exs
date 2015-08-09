56b294cdc84bca1fa962573df823242d2f8a1060
Ryo IEDA <ieda@val.co.jp>
2015-08-09 15:20:57 +0900

    デプロイ用設定修正

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,7 +7,7 @@ config :hello_phoenix, HelloPhoenix.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Endpoint,
+config :hello_phoenix, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the pool
